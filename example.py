8a206894723fa30cdeb05f89afb87814c54ea50b
NurdauletAlibay <alibay.n01@mail.ru>
2022-10-19 15:45:43 +0600

    example

---
+++ example.py
@@ -0,0 +1,29 @@
+'''
+s = input()
+q = s.find('t')
+print(q)
+print(len(s))
+print(s[1:5])
+n = input()
+print(s+n)
+'''
+
+'''
+a = "12345645124"
+line = a.replace('12', ':')
+print(line)
+'''
+'''
+b = "At al anta"
+#k = b.lower()
+m = b.title()
+t = "a"
+count = k.count(t)
+print(count)
+'''
+'''
+c = "almaty keremet kala"
+print(c.capitalize())
+v = c.title()
+print(v)
+'''
